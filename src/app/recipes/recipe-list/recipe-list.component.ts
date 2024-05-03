3208bb787f326d0f2546524b3d93eb7bd99d3e69
MirAbbasAli <abbasalimir97@gmail.com>
2024-05-03 15:57:03 +0530

    Outputting list of component with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "Test desc", "https://thecozycook.com/wp-content/uploads/2022/04/Lasagna-Recipe-f.jpg"),
+    new Recipe("Recipe Two", "Test Desc", "https://hips.hearstapps.com/hmg-prod/images/shrimp-scampi-recipe-2-66194bae4e71a.jpg?crop=0.8888888888888888xw:1xh;center,top")
+  ];
+  
+  constructor(){}
 
 }
